37249f26f2babf621163519ea610959fd5ffa681
Eizeed <eizeed321@gmail.com>
2025-05-27 10:13:05 +0500

    a

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
+use std::time::Duration;
+
+use opencv::{highgui, imgcodecs, prelude::*};
+
 fn main() {
-    println!("Hello, world!");
+    let img = imgcodecs::imread("frieren7.jpg", imgcodecs::IMREAD_COLOR).unwrap();
+    highgui::imshow("window", &img).unwrap();
+    highgui::wait_key(0).unwrap();
 }
